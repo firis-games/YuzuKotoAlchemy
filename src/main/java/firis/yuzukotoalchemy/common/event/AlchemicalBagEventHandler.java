9deb84bd73dc09b031688bb02e7653ff6482f1ac
firis <firis.games.lapismoonlight@gmail.com>
2019-04-20 12:06:08 +0900

    錬金バッグ拡張2
    ・アイテムを回収時に特定条件を満たした場合に錬金バッグへの自動収納する機能追加

--- src/main/java/firis/yuzukotoalchemy/common/event/AlchemicalBagEventHandler.java
+++ src/main/java/firis/yuzukotoalchemy/common/event/AlchemicalBagEventHandler.java
@@ -1,5 +1,8 @@
 package firis.yuzukotoalchemy.common.event;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import moze_intel.projecte.api.ProjectEAPI;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumDyeColor;
@@ -8,8 +11,10 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
@@ -92,6 +97,80 @@ public class AlchemicalBagEventHandler {
 		}
 		//後続の処理を停止する
 		event.setCanceled(true);
+	}
+	
+	/**
+	 * 錬金バッグのアイテム回収
+	 * @param event
+	 */
+	@SubscribeEvent
+	public static void onEntityItemPickupEvent(EntityItemPickupEvent event) {
+		
+		EntityPlayer player = event.getEntityPlayer();
+		if (player == null) return;
+		
+		//Inventoryに錬金バッグがあるか確認
+		int idx = -1;
+		for (int i = 0; i < player.inventory.getSizeInventory(); i++) {
+			if (isAlchemicalBag(player.inventory.getStackInSlot(i))) {
+				idx = i;
+				break;
+			}			
+		}
+		
+		if (idx == -1) return;
+		
+		//錬金バッグが存在する場合
+		IItemHandler alchemicalBag = player.getCapability(
+				ProjectEAPI.ALCH_BAG_CAPABILITY, null)
+				.getBag(EnumDyeColor.byMetadata(player.inventory.getStackInSlot(idx).getItemDamage()));
+		
+		//錬金バッグの1列目のItemStackを取得(13こ)
+		List<ItemStack> checkStack = new ArrayList<ItemStack>();
+		for (int i = 0; i < 13; i++) {
+			ItemStack stack = alchemicalBag.getStackInSlot(i);
+			if (!stack.isEmpty()) {
+				boolean ret = false;
+				for(ItemStack check : checkStack) {
+					if (!ItemStack.areItemsEqual(stack, check)) {
+						ret = true;
+						break;
+					}
+				}
+				if (ret || checkStack.size() == 0) {
+					checkStack.add(stack.copy());
+				}
+			}			
+		}
+		
+		ItemStack stack = event.getItem().getItem().copy();
+		//チェック処理
+		boolean ret = false;
+		for (ItemStack check : checkStack) {
+			if (ItemStack.areItemsEqual(check, stack)) {
+				ret = true;
+				break;
+			}
+		}
+		
+		if (!ret) return;
+		
+		//錬金バッグへ挿入する
+		for (int i = 0; i < alchemicalBag.getSlots(); i++) {
+			stack = alchemicalBag.insertItem(i, stack, false);
+			if (stack.isEmpty()) {
+				break;
+			}
+		}
+		
+		//減らす数
+		int stackCount = event.getItem().getItem().getCount() 
+				- event.getItem().getItem().getCount() 
+				- stack.getCount();
+		
+		event.getItem().getItem().setCount(stackCount);
+		
+		event.setResult(Result.ALLOW);
 	}
 	
 }
