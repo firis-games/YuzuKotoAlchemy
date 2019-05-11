3bd1f7d5254d7663a22d063a2e87574d93090736
firis <firis.games.lapismoonlight@gmail.com>
2019-05-11 10:04:36 +0900

    ver0.2

--- src/main/java/firis/yuzukotoalchemy/YuzuKotoAlchemy.java
+++ src/main/java/firis/yuzukotoalchemy/YuzuKotoAlchemy.java
@@ -50,7 +50,7 @@ public class YuzuKotoAlchemy
 {
     public static final String MODID = "yuzukotoalchemy";
     public static final String NAME = "YuzuKoto Alchemy";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
     public static final String MOD_DEPENDENCIES = "required-after:forge@[1.12.2-14.23.5.2768,);required-after:projecte@[1.12,);after:jei@[1.12.2-4.13.1.220,)";
     public static final String MOD_ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2]";
 
